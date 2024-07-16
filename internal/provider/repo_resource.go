6382bbf34fb0f01511dea95afb1b9c97d5cdb6ce
Adrian Hardenacke <adrian.hardenacke@obs-solutions.com>
2024-07-16 09:35:49 +0200

    [UPD] typo fix

--- internal/provider/repo_resource.go
+++ internal/provider/repo_resource.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"os"
+	"strconv"
 	"terraform-provider-borgwarehouse/tools"
 )
 
@@ -183,7 +184,7 @@ func (r *repoResource) Create(ctx context.Context, req resource.CreateRequest, r
 		return
 	}
 
-	command := "command=\"cd /home/borgwarehouse/repos;borg serve --restrict-to-path /home/borgwarehouse/repos/" + convert.RepositoryName + " --storage-quota " + string(rune(convert.StorageSize)) + "G\",restrict " + convert.SSHPublicKey
+	command := "command=\"cd /home/borgwarehouse/repos;borg serve --restrict-to-path /home/borgwarehouse/repos/" + convert.RepositoryName + " --storage-quota " + strconv.Itoa(convert.StorageSize) + "G\",restrict " + convert.SSHPublicKey
 
 	execute := "echo '" + command + "' | tee -a /home/borgwarehouse/.ssh/authorized_keys >/dev/null"
 
